1d3d85ce4cef8837df35516c97d5e9a5338f048b
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-07-14 10:34:50 +0800

    Create architecture.menu.ts

---
+++ src/configs/menus/architecture.menu.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    icon: "mdi-chart-bubble",
+    text: "EchartAssociation",
+    link: "/chart/echart-association",
+    key: "menu.echartAssociation",
+  },
+  {
+    icon: "mdi-chart-donut-variant",
+    text: "EchartComposition",
+    link: "/chart/echart-composition",
+    key: "menu.echartComposition",
+  },
+];
