103a286d9e00a8efb394c49ad9a28c60949eee0e
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-07-25 17:20:43 +0800

    Update AuthLayout.vue

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="d-flex text-center flex-grow-1">
-    <v-sheet
-      class="layout-side d-none d-md-flex flex-column justify-space-between"
-    >
-      <div class="mt-3 mt-md-10 pa-2">
-        <div class="text-primary text-h4 font-weight-bold">
-          Lux Vuetify3 Admin
-        </div>
-        <div class="title my-2">
-          Welcome! Let's build amazing things together.
-        </div>
-      </div>
-    </v-sheet>
     <v-card
       variant="outlined"
       class="flex-grow-1 d-flex align-center justify-center flex-column"
