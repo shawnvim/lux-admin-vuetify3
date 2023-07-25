09c64425542d1435c37ccfef774f8c2d770bdcc7
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-07-25 17:18:50 +0800

    Create ToolbarLogin.vue

---
+++ src/components/toolbar/ToolbarLogin.vue
@@ -0,0 +1,90 @@
+<!--
+* @Component: ToolbarNotifications
+* @Maintainer: J.K. Yang
+* @Description:
+-->
+<script setup lang="ts">
+import StatusMenu from "./StatusMenu.vue";
+import { useAuthStore } from "@/stores/authStore";
+import { useRouter } from "vue-router";
+import { useProfileStore } from "@/stores/profileStore";
+
+const profileStore = useProfileStore();
+const router = useRouter();
+
+const authStore = useAuthStore();
+const handleLogin = () => {
+  authStore.login();
+  console.log("---");
+  console.log(router);
+};
+
+</script>
+
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    location="bottom right"
+    transition="slide-y-transition"
+  >
+    <!-- ---------------------------------------------- -->
+    <!-- Activator Btn -->
+    <!-- ---------------------------------------------- -->
+    <template v-slot:activator="{ props }">
+      <v-btn class="mx-2" icon v-bind="props">
+        <v-icon color="primary">mdi-account</v-icon>
+      </v-btn>
+    </template>
+    <v-card max-width="300">
+
+      <!-- ---------------------------------------------- -->
+      <!-- Menu Area -->
+      <!-- ---------------------------------------------- -->
+
+
+      <!-- ---------------------------------------------- -->
+      <!-- Logout Area -->
+      <!-- ---------------------------------------------- -->
+      <v-list variant="flat" elevation="0" :lines="false" density="compact">
+        <v-list-item color="primary" to="nav.link" link density="compact">
+          <template v-slot:prepend>
+            <v-avatar size="30">
+              <v-icon>mdi-lifebuoy</v-icon>
+            </v-avatar>
+          </template>
+
+          <div>
+            <v-list-item-subtitle class="text-body-2">
+              {{ $t("menu.helpCenter") }}
+            </v-list-item-subtitle>
+          </div>
+        </v-list-item>
+        <v-list-item
+          color="primary"
+          link
+          @click="handleLogin"
+          density="compact"
+        >
+          <template v-slot:prepend>
+            <v-avatar size="30">
+              <v-icon>mdi-login</v-icon>
+            </v-avatar>
+          </template>
+
+          <div>
+            <v-list-item-subtitle class="text-body-2">
+              {{ $t("login.title") }}
+            </v-list-item-subtitle>
+          </div>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-menu>
+</template>
+
+<style scoped lang="scss">
+// ::v-deep .v-list-item__append,
+// ::v-deep .v-list-item__prepend {
+//   height: 100%;
+// }
+</style>
