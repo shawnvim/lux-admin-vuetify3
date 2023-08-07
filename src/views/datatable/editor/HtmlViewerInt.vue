e719fe709c5192f8ba896bac3ad7733ad26d8502
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-08-07 09:16:26 +0800

    Create HtmlViewerInt.vue

---
+++ src/views/datatable/editor/HtmlViewerInt.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="h-full">
+    
+  </v-container>
+</template>
+  
+  
+<script setup lang="ts">
+const route = useRouter();
+import HtmlPanel from "@/components/HtmlPanel.vue";
+
+const url1 = ref('')
+
+onMounted(() => {
+  alert("gaga")
+    console.log('route', route)
+})
+
+
+//router.push(`/data/html-viewer/rfc7540`);
+
+//const url1 = ref('/rfc/rfc7540.html')
+
+</script>
