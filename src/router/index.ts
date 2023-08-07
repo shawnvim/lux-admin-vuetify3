b153cae41c10f252cc90bbdbc4e6e81ca21bebe3
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-08-07 09:20:39 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -57,6 +57,11 @@ const router = createRouter({
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+        };
     } else {
       return { top: 0 };
     }
