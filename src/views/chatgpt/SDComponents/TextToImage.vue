967bc09489c74ba59ce8a6e87ceab3473fa84bbf
Shawn Qiu <114594494+shawnvim@users.noreply.github.com>
2023-09-30 22:50:43 +0800

    Update TextToImage.vue

--- src/views/chatgpt/SDComponents/TextToImage.vue
+++ src/views/chatgpt/SDComponents/TextToImage.vue
@@ -4,84 +4,69 @@
 * @Description: 
 -->
 <script setup lang="ts">
-import { img2imgApi } from "@/api/stableDiffusionApi";
-import { useStableDiffusionStore } from "@/stores/stableDiffusionStore";
-const sdStore = useStableDiffusionStore();
+import { imageGenerationApi } from "@/api/openaiApi";
+import { useOpenaiStore } from "@/stores/openaiStore";
+import { useSnackbarStore } from "@/stores/snackbarStore";
+const snackbarStore = useSnackbarStore();
+const openaiStore = useOpenaiStore();
+
 const params = reactive({
-  seed: -1,
-  enable_hr: false,
-  height: 200,
-  negative_prompt: "",
-  prompt: "1cat",
-  width: 200,
-  steps: 20,
+  prompt: "a white siamese cat",
+  n: 1,
+  size: "1024x1024",
 });
 
+const marks = ref([
+      256,
+      512,
+      1024
+    ]);
+
+const value = ref(marks.value.length-1);
+
+watch(value, (newValue) => {
+  const pixel = marks.value[newValue];
+  params.size = `${pixel}x${pixel}`;
+});
+
+
 const imageGenerate = async () => {
-  const res = await img2imgApi(params);
-  sdStore.updateImgList(res.data.images);
+  try {
+    const res = await imageGenerationApi(params);
+    // Handle the successful response
+    openaiStore.updateImgList(res.data.data);
+    snackbarStore.showSuccessMessage("Image Generated, start loading");
+    console.log("ImageGenerationResult", res);
+  } catch (error) {
+    console.log(error)
+    // Handle the error
+    snackbarStore.showErrorMessage(error.message);
+  }
+
 };
 </script>
 
 <template>
-  <v-card height="800">
+  <v-card>
     <v-card-text>
       <!-- ---------------------------------------------- -->
       <!-- Prompt  -->
       <!-- ---------------------------------------------- -->
       <v-label class="font-weight-medium my-3">Prompt</v-label>
-      <v-textarea
-        v-model="params.prompt"
-        color="primary"
-        variant="outlined"
-        density="compact"
-        type="text"
-        placeholder="Prompt"
-        hide-details
-      />
+      <v-textarea v-model="params.prompt" color="primary" variant="outlined" density="compact" type="text"
+        placeholder="Prompt" hide-details />
 
-      <!-- ---------------------------------------------- -->
-      <!-- Negative Prompt  -->
-      <!-- ---------------------------------------------- -->
-      <v-label class="font-weight-medium my-3">Negative Prompt</v-label>
-      <v-textarea
-        v-model="params.negative_prompt"
-        color="primary"
-        variant="outlined"
-        density="compact"
-        type="text"
-        placeholder="Negative Prompt"
-        hide-details
-      />
-
-      <!-- ---------------------------------------------- -->
-      <!-- Width   -->
-      <!-- ---------------------------------------------- -->
-      <v-label class="font-weight-medium my-3">Width</v-label>
-      <v-slider
-        v-model="params.width"
-        thumb-label="always"
-        max="2480"
-        step="10"
-        color="primary"
-      ></v-slider>
       <!-- ---------------------------------------------- -->
       <!-- Height   -->
       <!-- ---------------------------------------------- -->
-      <v-label class="font-weight-medium my-3">Height</v-label>
-      <v-slider
-        v-model="params.height"
-        thumb-label="always"
-        max="2480"
-        step="10"
-        color="primary"
-      ></v-slider>
+      <v-label class="font-weight-medium my-3">Size {{ params.size }}</v-label>
+      <v-slider v-model="value" :max="2" step="1" color="primary">
+      </v-slider>
 
-      <v-btn size="x-large" color="primary" block @click="imageGenerate"
-        >Generate</v-btn
-      >
+      <v-btn size="x-large" color="primary" block @click="imageGenerate">Generate</v-btn>
     </v-card-text>
   </v-card>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
